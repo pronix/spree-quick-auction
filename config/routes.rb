9480b1f74e7e8e7a58fd94980e290247cc2ce471
Maxim Kremenev <ezotrank@gmail.com>
2010-02-08 00:43:10 +0200

    Design change. Start write a tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-map.root :controller => "quick_auctions", :action => "index"
+map.root :controller => "quick_auctions", :action => "index", :method => "get"
 map.change_price '/change_price', :controller => "quick_auctions", :action => "create"
-map.checkout_variant '/checkout/:permalink', :controller => "quick_auctions", :action => "checkout"
+map.checkout_variant '/checkout/:permalink', :controller => "quick_auctions", :action => "checkout", :method => "get"
 map.resources :quick_auctions
 
--- spec/controllers/quick_auctions_controller_spec.rb
+++ spec/controllers/quick_auctions_controller_spec.rb
@@ -2,9 +2,59 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe QuickAuctionsController do
 
-  #Delete this example and add some real ones
-  it "should use QuickAuctionsController" do
-    controller.should be_an_instance_of(QuickAuctionsController)
-  end
+  describe 'Test routes' do
+    
+    describe 'Test / path and routes' do
+      it "should / must be quick_auction index" do
+        root_path.should == '/'
+        params_from(:get, '/').should == { :controller => 'quick_auctions', :action => 'index', :method => "get" }
+        route_for(:controller => 'quick_auctions', :action => 'index', :method => 'get').should == '/'
+      end
+      it "should / must not work" do
+        params_from(:post, '/').should_not == { :controller => 'quick_auctions', :action => 'index'}
+        params_from(:delete, '/').should_not == { :controller => 'quick_auctions', :action => 'index'}
+        params_from(:put, '/').should_not == { :controller => 'quick_auctions', :action => 'index'}
+      end
+    end
+    
+    describe 'Test /checkout path and routes' do
+      it "should success with corrent options" do
+        checkout_variant_path(:permalink => 'abcd').should == '/checkout/abcd'
+        params_from(:get, '/checkout/abcd').should == { :controller => 'quick_auctions', 
+                                                        :action => 'checkout', 
+                                                        :method => "get",
+                                                        :permalink => 'abcd'}
+        route_for(:controller => 'quick_auctions', :action => 'checkout', 
+                  :method => 'get', :permalink => 'abcd').should == '/checkout/abcd'
+      end
+      it "should / must not work" do
+        params_from(:post, '/checkout/abcd').should_not == '/checkout/abcd'
+        params_from(:delete, '/checkout/abcd').should_not == '/checkout/abcd'
+        params_from(:put, '/checkout/abcd').should_not == '/checkout/abcd'
+        params_from(:get, '/checkout/abcd').should_not == { :controller => 'quick_auctions', 
+                                                            :action => 'checkout', 
+                                                            :method => "get",
+                                                            :permalink => 'bcda' }
+      end
+    end
+
+    describe 'Test controller actions' do
+
+      describe 'Check :controller => quick_auctions, :action => index' do
+        def do_action
+          get :index
+        end
+        
+        it "should be success" do
+          do_action
+          response.should be_success
+        end
+        
+      end 
+      
+    end 
+
+    
+  end 
 
 end
