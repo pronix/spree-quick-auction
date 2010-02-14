6fce1f4304ca71e40a83a89717dc76a28eb5da13
Maxim Kremenev <ezotrank@gmail.com>
2010-02-14 19:03:49 +0200

    some design fix and add comments

--- app/controllers/quick_auctions_controller.rb
+++ app/controllers/quick_auctions_controller.rb
@@ -19,8 +19,10 @@ class QuickAuctionsController < ApplicationController
   end
   
   def checkout
+    # Destroy latest order, becouse order must be only one
     Order.find(session[:order_id]).destroy rescue '' if session[:order_id]
     begin
+      # Find product variant by cookie
       @variant = Product.find_by_permalink(params[:permalink]).variants.find(cookies[:selected_option])
       if @variant.nil? || !@variant.in_stock? || !@variant.product.available?
         flash[:notice] = "Sorry, but the lot has already been bought or time is expired"
